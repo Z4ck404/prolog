3bd914f2204f51a6eba3fbd04b209fbe8c808a1e
Z4ck404 <35115877+Z4ck404@users.noreply.github.com>
2018-05-13 17:18:30 +0000

    Update NLPF.pl

--- NLPF.pl
+++ NLPF.pl
@@ -1,6 +1,6 @@
 %ce programme prolog recoaanit la structure languistique d'une phrase
 %et corrige aussi les fautes d'orthographe.
-%La grammaire que les phrases doivent respectées :
+%La grammaire que les phrases doivent respectÃ©es :
 ph(P,R,_,_):-gn(P,S,_,_),gv(S,R,_,_).
 ph([],[],_,_).
 %le groupe nominal :
@@ -23,11 +23,11 @@ verbeetat([V|R],R,f,s):-member(V,["suis","es","est"]),write(verbe_etat__).
 verbeetat([V|R],R,f,p):-member(V,["sommes","etes","sont"]),write(verbe_etat__).
 cod(P,S,_,_):-sn(P,S,_,_),write(cod__).
 cod(P,P,_,_).
-coi(["à"|R],S,_,_):-sn(R,S,_,_),write(coi__).
+coi(["Ã "|R],S,_,_):-sn(R,S,_,_),write(coi__).
 coi(P,P,_,_).
 ccir(P,R,_,_):-prep(P,R1),sn(R1,R,_,_),write(ccir__).
 ccir(P,P,_,_).
-prep([P|R],R):-member(P,["à","de","dans","avec"]),write(preposition__).
+prep([P|R],R):-member(P,["Ã ","de","dans","avec"]),write(preposition__).
 prep(P,P).
 det([D|R],R,G,N):-detm(D,G,N),write(determinat__).
 det(P,P,_,_).
@@ -37,7 +37,7 @@ detm(D,G,N):-member(D,["les","des"]),G=m,N=p.
 detm(D,G,N):-member(D,["les","des"]),G=f,N=p.
 attribut(R,S,_,_):-sn(R,S,_,_).
 attribut(R,S,G,N):-adjs(R,S,G,N).
-cnom(["de"|N],R,G,N):-G,N,sn(N,R,_,_),write(complementNom__).
+cnom(["de"|N],R,G,N):-sn(N,R,G,N),write(complementNom__).
 cnom(R,R,_,_).
 adjs([A|R],S,G,N):-adj(A,G,N),adjs(R,S,G,N).
 adjs(P,P,_,_).
